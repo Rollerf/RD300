aae88db99417bf020ed29279dd85db0cc7bae90b
Chema <rollerfdeveloper@gmail.com>
2023-02-27 12:52:02 +0100

    fix: Reduce number of mqtt messages

--- main.ino
+++ main.ino
@@ -15,7 +15,7 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-//ESTACIONES:
+// ESTACIONES:
 bool E0 = false;
 bool E1 = false;
 bool E2 = false;
@@ -29,28 +29,30 @@ bool E8 = false;
 bool E9 = false;
 bool E10 = false;
 
-//TIMERS:
-TON* tPublishInfo;
-TON* tSerial;
-TON* tCheckConnection;
+// TIMERS:
+TON *tPublishInfo;
+TON *tSerial;
+TON *tCheckConnection;
 
-//CONSTANTES
-char* CERRADO = "cerrado";
-char* ABIERTO = "abierto";
-char* INTERMEDIO = "intermedio";
-char* ERROR_FINALES_CARRERA = "Error finales carrera";
+// CONSTANTES
+char *CERRADO = "cerrado";
+char *ABIERTO = "abierto";
+char *INTERMEDIO = "intermedio";
+char *ERROR_FINALES_CARRERA = "Error finales carrera";
 
-char* ABRIR = "abrir";
-char* CERRAR = "cerrar";
-char* PARAR = "parar";
+char *ABRIR = "abrir";
+char *CERRAR = "cerrar";
+char *PARAR = "parar";
 
 const boolean START = true;
 const boolean RESET = false;
 
-//VARIABLES
+// VARIABLES
 static bool recorridoGuardado = false;
+char *estado = "";
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   WIFIConnection();
@@ -59,7 +61,7 @@ void setup() {
 
   setup_entradas();
 
-  //Init limit switchs
+  // Init limit switchs
   Serial.print("antireboteFC: ");
   Serial.println(antireboteFC());
 
@@ -79,25 +81,42 @@ void setup() {
   tCheckConnection = new TON(52000);
 }
 
-void publishInfo() {
-  if (tPublishInfo->IN(START)) {
+char *getEstado()
+{
+  if (antireboteFC() && !antireboteFA())
+  {
+    return CERRADO;
+  }
+  if (antireboteFA() && !antireboteFC())
+  {
+    return ABIERTO;
+  }
+  if (!antireboteFA() && !antireboteFC())
+  {
+    return INTERMEDIO;
+  }
+  if (antireboteFC() && antireboteFA())
+  {
+    return ERROR_FINALES_CARRERA;
+  }
+}
+
+void publishInfo()
+{
+  if (tPublishInfo->IN(START))
+  {
     StaticJsonDocument<192> jsonDoc;
     JsonObject recorrido = jsonDoc.createNestedObject("recorrido");
 
     String payload = "";
-    char* estado = "";
 
-    if (antireboteFC() && !antireboteFA()) {
-      estado = CERRADO;
-    } else if (antireboteFA() && !antireboteFC()) {
-      estado = ABIERTO;
-    } else if (!antireboteFA() && !antireboteFC()) {
-      estado = INTERMEDIO;
+    if ((estado == CERRADO || estado == ABIERTO || estado == ERROR_FINALES_CARRERA) && estado == getEstado())
+    {
+      tPublishInfo->IN(RESET);
+      return;
     }
 
-    if (antireboteFC() && antireboteFA()) {
-      estado = ERROR_FINALES_CARRERA;
-    }
+    estado = getEstado();
 
     jsonDoc["estado"] = estado;
     jsonDoc["consumo"] = getConsumo();
@@ -107,16 +126,18 @@ void publishInfo() {
     recorrido["t_PosicionFinal"] = getPosicionFinal();
 
     serializeJson(jsonDoc, payload);
-    client.publish(topicState, (char*)payload.c_str());
+    client.publish(topicState, (char *)payload.c_str());
 
     tPublishInfo->IN(RESET);
   }
-
 }
 
-void checkMqttConnection() {
-  if (tCheckConnection->IN(START)) {
-    if (!client.connected()) {
+void checkMqttConnection()
+{
+  if (tCheckConnection->IN(START))
+  {
+    if (!client.connected())
+    {
       ESP.restart();
     }
 
@@ -132,9 +153,10 @@ void loop()
 
   publishInfo();
 
-  if (E0) //Posicion standby
+  if (E0) // Posicion standby
   {
-    if (tSerial->IN(START)) {
+    if (tSerial->IN(START))
+    {
       Serial.println("E0");
 
       tSerial->IN(RESET);
@@ -152,7 +174,8 @@ void loop()
 
   if (E1)
   {
-    if (tSerial->IN(START)) {
+    if (tSerial->IN(START))
+    {
       Serial.println("E1");
 
       tSerial->IN(RESET);
@@ -168,14 +191,14 @@ void loop()
     if (antireboteFC() && !antireboteFA())
     {
       E3 = true;
-      C_Tiempos(1);//Tiempo inicial
+      C_Tiempos(1); // Tiempo inicial
       E1 = false;
     }
 
     if (!antireboteFC() && antireboteFA())
     {
       E5 = true;
-      C_Tiempos(1);//Tiempo inicial
+      C_Tiempos(1); // Tiempo inicial
       E1 = false;
     }
 
@@ -184,14 +207,15 @@ void loop()
       E0 = true;
     }
 
-    T1(0, 0); //Reseteo el temporizador
-    antiaplastamiento(false);//Reseteamos valores de antiaplastamiento
-    //Serial.println("E1");
+    T1(0, 0);                 // Reseteo el temporizador
+    antiaplastamiento(false); // Reseteamos valores de antiaplastamiento
+    // Serial.println("E1");
   }
 
-  if (E2) //POSICION INTERMEDIA
+  if (E2) // POSICION INTERMEDIA
   {
-    if (tSerial->IN(START)) {
+    if (tSerial->IN(START))
+    {
       Serial.println("E2");
 
       tSerial->IN(RESET);
@@ -207,12 +231,12 @@ void loop()
       E2 = false;
       Serial.println("E2");
     }
-
   }
 
-  if (E3) //ABRIR
+  if (E3) // ABRIR
   {
-    if (tSerial->IN(START)) {
+    if (tSerial->IN(START))
+    {
       Serial.println("E3");
       //
       //      Serial.print("Wifi: ");
@@ -243,14 +267,15 @@ void loop()
     {
       E0 = true;
       E3 = false;
-      //Serial.println("E3");
+      // Serial.println("E3");
     }
     C_Tiempos(2);
   }
 
-  if (E5) //CERRAR
+  if (E5) // CERRAR
   {
-    if (tSerial->IN(START)) {
+    if (tSerial->IN(START))
+    {
       Serial.println("E5");
 
       tSerial->IN(RESET);
@@ -274,14 +299,15 @@ void loop()
     {
       E0 = true;
       E5 = false;
-      //Serial.println("E5");
+      // Serial.println("E5");
     }
     C_Tiempos(2);
   }
 
-  if (E4)//Posicion standby
+  if (E4) // Posicion standby
   {
-    if (tSerial->IN(START)) {
+    if (tSerial->IN(START))
+    {
       Serial.println("E4");
       //      Serial.print("Recorrido guardado: ");
       //      Serial.println(recorridoGuardado);
@@ -297,26 +323,27 @@ void loop()
 
     if (!recorridoGuardado)
     {
-      C_Tiempos(3);// Hago el calculo de los tiempos Rapida y Seguridad
+      C_Tiempos(3); // Hago el calculo de los tiempos Rapida y Seguridad
       recorridoGuardado = true;
     }
 
     parar();
     checkMqttConnection();
 
-    T1(0, 0); //Reseteo T1
-    antiaplastamiento(false);//Reseteamos valores de antiaplastamiento
+    T1(0, 0);                 // Reseteo T1
+    antiaplastamiento(false); // Reseteamos valores de antiaplastamiento
     if (recibir())
     {
       E6 = true;
       E4 = false;
-      //Serial.println("E4");
+      // Serial.println("E4");
     }
   }
 
   if (E6)
   {
-    if (tSerial->IN(START)) {
+    if (tSerial->IN(START))
+    {
       Serial.println("E6");
 
       tSerial->IN(RESET);
@@ -325,10 +352,10 @@ void loop()
     static bool e_abrir = false;
     static bool e_cerrar = false;
 
-    if (!antireboteFC() && !antireboteFA()) //INTERMEDIO
+    if (!antireboteFC() && !antireboteFA()) // INTERMEDIO
     {
 
-      if (e_abrir) //Estaba abriendo asi que cerramos
+      if (e_abrir) // Estaba abriendo asi que cerramos
       {
         C_Tiempos(1);
         E7_c = true;
@@ -337,7 +364,7 @@ void loop()
         E6 = false;
       }
 
-      else if (e_cerrar) //Estaba cerrando asi que abrimos
+      else if (e_cerrar) // Estaba cerrando asi que abrimos
       {
         C_Tiempos(1);
         E7_a = true;
@@ -347,28 +374,28 @@ void loop()
       }
     }
 
-    if (antireboteFC() && !antireboteFA()) //ABRIR
+    if (antireboteFC() && !antireboteFA()) // ABRIR
     {
       e_cerrar = false;
       e_abrir = true;
       E8 = true;
-      C_Tiempos(1);//Tiempo inicial
+      C_Tiempos(1); // Tiempo inicial
       E6 = false;
-      //Serial.println("E6 ABRIR");
+      // Serial.println("E6 ABRIR");
     }
 
-    if (!antireboteFC() && antireboteFA()) //CERRAR
+    if (!antireboteFC() && antireboteFA()) // CERRAR
     {
       e_abrir = false;
       e_cerrar = true;
       E9 = true;
-      C_Tiempos(1);//Tiempo inicial
+      C_Tiempos(1); // Tiempo inicial
       E6 = false;
-      //Serial.println("E6 CERRAR");
+      // Serial.println("E6 CERRAR");
     }
   }
 
-  if (E7_a) //INTERMEDIO ABRIR
+  if (E7_a) // INTERMEDIO ABRIR
   {
     if (T1(1, 1000))
     {
@@ -399,7 +426,7 @@ void loop()
     }
   }
 
-  if (E7_c) //INTERMEDIO CERRAR
+  if (E7_c) // INTERMEDIO CERRAR
   {
     if (T1(1, 1000))
     {
@@ -429,9 +456,10 @@ void loop()
     }
   }
 
-  if (E8) //ABRIR
+  if (E8) // ABRIR
   {
-    if (tSerial->IN(START)) {
+    if (tSerial->IN(START))
+    {
       Serial.println("E8");
 
       tSerial->IN(RESET);
@@ -466,9 +494,10 @@ void loop()
     }
   }
 
-  if (E9) //CERRAR
+  if (E9) // CERRAR
   {
-    if (tSerial->IN(START)) {
+    if (tSerial->IN(START))
+    {
       Serial.println("E9");
 
       tSerial->IN(RESET);
@@ -500,15 +529,16 @@ void loop()
       E4 = true;
       E9 = false;
     }
-
   }
 }
 
-void WIFIConnection() {
+void WIFIConnection()
+{
   // connecting to a WiFi network
   WiFi.begin(ssid, password);
 
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
     Serial.println("Connecting to WiFi..");
     delay(5000);
     ESP.restart();
@@ -518,9 +548,11 @@ void WIFIConnection() {
   WiFi.persistent(true);
 }
 
-void OTAConfig() {
+void OTAConfig()
+{
   ArduinoOTA.setHostname(client_name);
-  ArduinoOTA.onStart([]() {
+  ArduinoOTA.onStart([]()
+                     {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
@@ -529,15 +561,13 @@ void OTAConfig() {
     }
 
     // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.println("Start updating " + type); });
+  ArduinoOTA.onEnd([]()
+                   { Serial.println("\nEnd"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+  ArduinoOTA.onError([](ota_error_t error)
+                     {
     Serial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) {
       Serial.println("Auth Failed");
@@ -549,23 +579,27 @@ void OTAConfig() {
       Serial.println("Receive Failed");
     } else if (error == OTA_END_ERROR) {
       Serial.println("End Failed");
-    }
-  });
+    } });
   ArduinoOTA.begin();
 }
 
-void MQTTConnection() {
-  //connecting to a mqtt broker
+void MQTTConnection()
+{
+  // connecting to a mqtt broker
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
 
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     String client_id = client_name;
     client_id += String(WiFi.macAddress());
     Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
-    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
+    {
       Serial.println("Public emqx mqtt broker connected");
-    } else {
+    }
+    else
+    {
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
@@ -575,14 +609,16 @@ void MQTTConnection() {
   client.subscribe(topicCommand);
 }
 
-void callback(char *topicCommand, byte *payload, unsigned int length) {
+void callback(char *topicCommand, byte *payload, unsigned int length)
+{
   //  Serial.print("Message arrived in topic: ");
   //  Serial.println(topicCommand);
   //  Serial.print("Message:");
   String payload_n;
 
-  for (int i = 0; i < length; i++) {
-    payload_n += (char) payload[i];
+  for (int i = 0; i < length; i++)
+  {
+    payload_n += (char)payload[i];
   }
 
   //  Serial.println(payload_n);
@@ -590,23 +626,27 @@ void callback(char *topicCommand, byte *payload, unsigned int length) {
 
   StaticJsonDocument<256> doc;
   DeserializationError error = deserializeJson(doc, payload_n);
-  if (error) return;
+  if (error)
+    return;
   String orden = doc["orden"];
   int consumoLimite = doc["consumoLimite"];
   boolean recorridoGuardadoRecibido = doc["recorrido"]["recorridoGuardado"];
 
-  if (orden == ABRIR || orden == CERRAR  || orden == PARAR) {
+  if (orden == ABRIR || orden == CERRAR || orden == PARAR)
+  {
     setAccionar(true);
   }
 
-  if (consumoLimite != 0) {
+  if (consumoLimite != 0)
+  {
     setConsumoLimite(consumoLimite);
   }
 
   recorridoGuardado = recorridoGuardadoRecibido;
   setPosicionFinal(doc["recorrido"]["t_PosicionFinal"]);
 
-  if (recorridoGuardado && getPosicionFinal() > 0) {
+  if (recorridoGuardado && getPosicionFinal() > 0)
+  {
     calcularTiempos();
     E4 = true;
     E0 = false;
